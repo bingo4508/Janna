7b446d38455fa14e20ccb9b32e6aa3a5afe37857
Chia-Lun Wu <chia@alloy.ai>
2020-09-14 22:44:43 +0200

    Updates

---
+++ precache-manifest.ed34a597791ea17cafcf5e3b5866e0c0.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0a35d47893b618d95464e22bbc5f06d8",
+    "url": "/Janna/index.html"
+  },
+  {
+    "revision": "34c335031b34457b1ab3",
+    "url": "/Janna/static/css/main.4117dc3b.chunk.css"
+  },
+  {
+    "revision": "7d6a0787ddd02e2c284b",
+    "url": "/Janna/static/js/2.a3698491.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Janna/static/js/2.a3698491.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "34c335031b34457b1ab3",
+    "url": "/Janna/static/js/main.0ee8ead0.chunk.js"
+  },
+  {
+    "revision": "6b8865c0a2a4be59827b",
+    "url": "/Janna/static/js/runtime-main.79d48003.js"
+  },
+  {
+    "revision": "de2d11ffbb7214085f9f873b31c97bc3",
+    "url": "/Janna/static/media/kiss1.de2d11ff.m4a"
+  },
+  {
+    "revision": "19e706c9f78cf433a91f3b7331ec1985",
+    "url": "/Janna/static/media/kiss2.19e706c9.m4a"
+  },
+  {
+    "revision": "66ce72ae3119dccc64e81da357ce1a9f",
+    "url": "/Janna/static/media/kiss3.66ce72ae.m4a"
+  },
+  {
+    "revision": "55a517daef50ad2965fb5843f5db7fa2",
+    "url": "/Janna/static/media/kiss4.55a517da.m4a"
+  }
+]);
